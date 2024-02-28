281bec9eca03a216b559c0caf6f1fff9459e3a40
Giles Falkingham <gfalkingham@tuta.com>
2024-02-28 01:52:10 +0000

    [GHF-03] Added functionality to chatbot so that it accepts a recursive directory structure to upload for analysis.

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import fnmatch
 import os
 import sys
 import threading
@@ -25,13 +26,68 @@ def get_user_input() -> str:
     user_input = prompt(print_formatted_text(text), multiline=True)
     return user_input
 
-def detect_file_analysis_request(content: str) -> (bool, str):
-    # Detects if the user is asking for a file analysis and extracts the file path.
+def detect_file_analysis_request(content: str) -> (bool, str, bool):
     if content.startswith("Upload:"):
-        file_path = content[len("Upload: "):].strip()
-        file_path = os.path.expanduser(file_path)
-        return True, file_path
-    return False, ""
+        path = content[len("Upload: "):].strip()
+        path = os.path.expanduser(path)
+        if os.path.isdir(path):
+            return True, path, True  # Indicates a directory
+        return True, path, False  # Indicates a file
+    return False, "", False
+
+def should_ignore(file_path):
+    ignore_patterns = [
+        '*/.terraform/*', '.terraform',
+        '*/.terragrunt-cache/*', '.terragrunt-cache',
+        '*.tfstate', '*.tfstate*',
+        '*/.tfsec/*', '.tfsec',
+        '.vmc-makefile', '*/.centralized-makefile',
+        'Pipfile', '*/Pipfile', 'Pipfile.lock', '*/Pipfile.lock',
+        '.test-plans', '*/.test-plans', '.cache', '*/.cache',
+        '*.pyc', '*/*.pyc', '*.pyo', '*/*.pyo', '*.zip', '*/*.zip',
+        '__pycache__', '*/__pycache__', '.tox', '*/.tox',
+        '*.egg-info', '*/*.egg-info', '.coverage', '*/.coverage',
+        '.pytest_cache', '*/.pytest_cache', 'nosetests.xml', '*/nosetests.xml',
+        'coverage.xml', '*/coverage.xml', 'htmlcov/', '*/htmlcov/',
+        'report.xml', '*/report.xml', 'build/*', '*/build/*', 'dist/*',
+        '*/dist/*', 'test-generated*.yml', '*/test-generated*.yml',
+        '.DS_Store', '._.DS_Store', '.librarian', '.idea', '.vscode',
+        '.history', '*swp', '.envrc', '.direnv', '.editorconfig',
+        '.external_modules', 'modules/*', '.terraform.lock.hcl', '*.png',
+        '*.jpg', '*.jpeg', '*.bmp', '.test-data', '*.plan', '*plan.out',
+        '*plan.summary', '*/.git', '*/.gitignore', '*/.git-credentials',
+        '*/manifest.json', '.checkov.yaml', '*/saml/*'
+    ]
+    for pattern in ignore_patterns:
+        if fnmatch.fnmatch(file_path, pattern):
+            return True
+    return False
+
+def is_binary(file_path):
+    try:
+        with open(file_path, 'rb') as f:
+            chunk = f.read(1024)  # Read the first 1024 bytes
+            return b'\x00' in chunk  # Look for a NULL byte
+    except Exception:
+        return True  # If there's an error reading the file, treat it as binary
+
+def generate_markdown_from_directory(root_dir) -> str:
+    markdown_output = ""
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        dirnames[:] = [d for d in dirnames if not should_ignore(os.path.join(dirpath, d))]
+        for filename in filenames:
+            file_path = os.path.join(dirpath, filename)
+            relative_file_path = os.path.relpath(file_path, start=root_dir)
+            if not should_ignore(file_path) and not is_binary(file_path):
+                with open(file_path, 'r', encoding='utf-8', errors='ignore') as file:
+                    content = file.read()
+                    # Determine the appropriate enclosure based on the file extension
+                    enclosure = "```"
+                    if filename.endswith('.md'):
+                        enclosure = '"""'
+                    
+                    markdown_output += f"## {relative_file_path}\n\n{enclosure}\n{content}\n{enclosure}\n\n"
+    return markdown_output
 
 def read_file_contents(file_path: str) -> (str, str, int):
     try:
@@ -112,23 +168,38 @@ To exit gracefully simply submit the word: "exit", or hit Ctrl+C.
             if should_exit(content):
                 break
 
-            is_file_request, file_path = detect_file_analysis_request(content)
+            is_file_request, path, is_directory = detect_file_analysis_request(content)
             if is_file_request:
-                file_name, file_contents, token_count = read_file_contents(file_path)
-                if file_contents == "FILE TOO BIG.":
-                    print(f"\nThe file: {file_name} is too large to upload because it is likely larger than 64,000 tokens.\n"
-                          f"Estimated token count for this file: {token_count}\n")
-                elif file_contents:
-                    analysis_request = (f"Please analyse the contents of the following file:\n"
-                                        f"\n{file_name}\n"
-                                        f"\n{file_contents}\n"
-                                        f"\nEnd your response by asking the user what questions they have about the file.")
-                    append_message(messages, "user", analysis_request)
-                    print(f"\nEstimated token count for this file: {token_count}\n")
+                if is_directory:
+                    markdown_content = generate_markdown_from_directory(path)
+                    if markdown_content:
+                        dir_analysis_request = (f"The following describes a directory stucture along with all its contents in "
+                                            f"Markdown format. "
+                                            f"Please carefully analyse the directory structure and the files contained within. Pay "
+                                            f"attention to whether the directory stucture looks like a code repository. Then take a "
+                                            f"deep breath and provide a brief summary of your analysis. End your response with an "
+                                            f"assurance that you have memorised the contents of the repository and you are ready to "
+                                            f"answer the user's questions.\n\n{markdown_content}")
+                        append_message(messages, "user", dir_analysis_request)
+                    else:
+                        print_formatted_text(HTML("<ansired>Directory is empty or contains no readable files.</ansired>"))
+                        continue
                 else:
-                    print_formatted_text(HTML(f"\nThe file: {file_name} is empty.\n"))
-                    print(f"Estimated token count for this file: {token_count}\n")
-                    continue
+                    file_name, file_contents, token_count = read_file_contents(path)
+                    if file_contents == "FILE TOO BIG.":
+                        print(f"\nThe file: {file_name} is too large to upload because it is likely larger than 64,000 tokens.\n"
+                              f"Estimated token count for this file: {token_count}\n")
+                    elif file_contents:
+                        file_analysis_request = (f"Please analyse the contents of the following file:\n"
+                                            f"\n{file_name}\n"
+                                            f"\n{file_contents}\n"
+                                            f"\nEnd your response by asking the user what questions they have about the file.")
+                        append_message(messages, "user", file_analysis_request)
+                        print(f"\nEstimated token count for this file: {token_count}\n")
+                    else:
+                        print_formatted_text(HTML(f"\nThe file: {file_name} is empty.\n"))
+                        print(f"Estimated token count for this file: {token_count}\n")
+                        continue
             else:
                 append_message(messages, "user", content)
 
